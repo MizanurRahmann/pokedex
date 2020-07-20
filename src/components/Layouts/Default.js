a64b3251230c23c625d91d9350d10047d15b9fcf
MizanurRahmann <mr926560@gmail.com>
2020-07-20 19:20:41 +0600

    Added JSX for Default page.

--- src/components/Layouts/Default.js
+++ src/components/Layouts/Default.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import '../../decorations/styles/css/Default.css';
 
 function Default() {
     return (
-        <div>
-            
+        <div className="container page-not-found">
+            <div className="fourOfour">
+                <div className=" middle">
+                    <h1>4</h1>
+                    <img src={require('../../decorations/images/poke-dribbble.png')} alt="logo" />
+                    <h1>4</h1>
+                </div>
+            </div>
+            <div className="line"></div>
+            <div className="fourOfour-message">
+                <div className="message">
+                    <p>
+                        Unfortunately something went wrong. Please double check the url or go back to the
+                    <span>landing page</span>. If you continue get this error - please email me at mr926560@gmial.com
+                </p>
+                    <button>Go Back</button>
+                </div>
+            </div>
         </div>
     )
 }
